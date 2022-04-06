3ef27b771aa3ad249d69d669f9780c8bd8a9648f
NoroMedafe <danya1515@inbox.ru>
2022-04-06 14:39:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            string auxiliaryVariable;
+
+            firstName = "Алексей";
+            Console.WriteLine("Имя:"+firstName) ;
+            lastName = "Алексеев";
+            Console.WriteLine("Фамилия:" + lastName + "\n");
+            auxiliaryVariable = firstName;
+            firstName = lastName;
+            lastName = auxiliaryVariable;
+            Console.WriteLine("Имя:" + firstName);
+            Console.WriteLine("Фамилия:" + lastName);
+            Console.ReadKey();
+        }
+    }
+}
